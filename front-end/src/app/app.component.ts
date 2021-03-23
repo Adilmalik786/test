a9f97c36d7536ddf112c3b45ca924a9f95f1970f
Adil <adilmalik.infinity@gmail.com>
2021-03-23 14:41:51 +0500

    Working on FrontEnd

--- front-end/src/app/app.component.ts
+++ front-end/src/app/app.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { QuestionClass } from './question-class';
+import { ModalDirective } from 'ngx-bootstrap/modal';
+// import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +9,69 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'front-end';
+  public isQuestionCardShow = false;
+	public totalAnswered = 0;
+	public rightAnswer: number;
+	questionObj = new QuestionClass();
+	@ViewChild('submitModal')
+  submitModal!: ModalDirective;
+	@ViewChild('addQuestionModal')
+  addQuestionModal!: ModalDirective;
+	@ViewChild('answerModal')
+  answerModal!: ModalDirective;
+	@ViewChild('questionForm') questionForm: any;
+	@ViewChild('questionTest') questionTest : any;
+
+  constructor() {
+    this.rightAnswer = 0;
+   }
+   allQuestions: any = [{
+		"id": 1,
+		"question": "What is the capital of Belgium?",
+		"a": "Vienna",
+		"b": "Berlin",
+		"c": "Brussels",
+		"d": "Prague",
+		"answer": "c"
+	},
+	{
+		"id": 2,
+		"question": "What is the capital of Australia?",
+		"a": "Vienna",
+		"b": "Canberra",
+		"c": "Brussels",
+		"d": "Prague",
+		"answer": "b"
+	},
+	{
+		"id": 3,
+		"question": "What is the capital of Bulgaria?",
+		"a": "Vienna",
+		"b": "Sofia",
+		"c": "Brussels",
+		"d": "Prague",
+		"answer": "b"
+	}
+	];
+   addQuestion(){
+		this.addQuestionModal.show();
+	}
+   startQuiz() {
+		for (let i = 0; i < this.allQuestions.length; i++) {
+			if ("selected" in this.allQuestions[i]) {
+				delete this.allQuestions[i]["selected"];
+			}
+
+		}
+		this.questionTest.reset();
+		this.isQuestionCardShow = true;
+
+	}
+  HomePage(){
+
+  }
+  createTest(){
+    
+  }
+  submitTest(){}
 }
--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -1,8 +1,16 @@
-import { NgModule } from '@angular/core';
+
 import { BrowserModule } from '@angular/platform-browser';
+
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NgModule } from '@angular/core';
+import { MaterialModule } from './material.module';
+import { ModalModule } from 'ngx-bootstrap/modal';
+import { ToastrModule } from 'ngx-toastr';
+
 
 @NgModule({
   declarations: [
@@ -10,8 +18,14 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ToastrModule,
+    HttpClientModule,
+    ModalModule,
+    MaterialModule
   ],
+  exports:[MaterialModule],
   providers: [],
   bootstrap: [AppComponent]
 })
